58d7f5e0e1c4ce84151e8f47d79aa39fb2f188a0
stuart540 <114927873+stuart540@users.noreply.github.com>
2023-01-21 17:32:02 +0000

    added Pseudo Code to JS

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+// The app should:
+
+// * Display the current day at the top of the calender when a user opens the planner.
+// PC use moment to access the current day subtitle
+ 
+// * Present timeblocks for standard business hours when the user scrolls down.
+// PC create a 9 row table template from bootstrap
+/* <div class="table-responsive">
+          <table class="table table-bordered custom-table"></table> */
+ 
+// * Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+// PC dynamically colour the background of the table according to the time from moment
+ 
+// * Allow a user to enter an event when they click a timeblock
+// PC Use jquery to dynamically update the table contents from the user
+
+// * Save the event in local storage when the save button is clicked in that timeblock.
+// PC set a button event listener for the save function, when clicked, the information will be stored to client local storage
+
+// * Persist events between refreshes of a page
